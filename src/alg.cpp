4a98efe18b381cedbfcb3b463ee640904a7ea421
LeraShmyrova <94710005+LeraShmyrova@users.noreply.github.com>
2022-02-23 15:40:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -9,9 +9,8 @@ bool checkPrime(uint64_t value) {
    return false;
   }
  }
-   return true;  
+   return true;
 }
-
 uint64_t nPrime(uint64_t n) {
  int i = 0;
  do {
@@ -22,7 +21,6 @@ uint64_t nPrime(uint64_t n) {
  } while( num < n );
  return i;
 }
-
 uint64_t nextPrime(uint64_t value) {
  int count = 0;
   while (count < 10) {
@@ -33,12 +31,11 @@ uint64_t nextPrime(uint64_t value) {
    value += 1;
   }
 }
-
 uint64_t sumPrime(uint64_t hbound) {
  for (int i == 2; i < hbound; i++) {
   if checkPrime(i) == True) {
    summ += i;
-  } 
- } 
+  }
+ }
  return summ;
 }
